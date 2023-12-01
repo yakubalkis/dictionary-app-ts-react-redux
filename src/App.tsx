ba764eed30262361196d6924f02cd5aa8a1300dc
yakubalkis <alkis.yakub@gmail.com>
2023-12-01 18:53:29 +0300

    added Main component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useAppSelector } from './redux/hooks';
 import Header from './components/Header';
 import Input from './components/Input';
+import Main from './components/Main';
 
 function App() {
   const modeType = useAppSelector(state => state.mode);
@@ -9,6 +10,7 @@ function App() {
       <div className={`App ${modeType.mode}-mode`}>
         <Header />
         <Input />
+        <Main />
       </div>
   );
 }
