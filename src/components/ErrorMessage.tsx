c19daa235f3bc54c34b74de94919a41f59f31d71
yakubalkis <alkis.yakub@gmail.com>
2023-12-01 23:48:26 +0300

    created error message component.

---
+++ src/components/ErrorMessage.tsx
@@ -0,0 +1,13 @@
+
+type ErrorMessageProps = {
+    text: string,
+  };
+  
+  export default function ErrorMessage({ text }: ErrorMessageProps) {
+    return (
+      <aside id="error">
+        <h1>An error occurred!</h1>
+        <p>{text}</p>
+      </aside>
+    );
+  }
